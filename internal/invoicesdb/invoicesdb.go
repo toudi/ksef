b126471ee27255f18cafec32db0ac733afdbe795
Mateusz Mikołajczyk <mikolajczyk.mateusz@gmail.com>
2025-12-31 17:56:11 +0100

    fix bug with calculation of previous month which would have led to duplicated invoices upload

--- internal/invoicesdb/invoicesdb.go
+++ internal/invoicesdb/invoicesdb.go
@@ -59,6 +59,14 @@ func newInvoicesDB(vip *viper.Viper) *InvoicesDB {
 	// just so that we don't have to call time.Now() time and time again
 	today := time.Now()
 	previousMonth := today.AddDate(0, -1, 0)
+	// the above line should take care for the most part, however there are
+	// edge cases like the 31'st day of the month. Go's time.AddDate does warn
+	// about this (i.e. when you call AddDate(0, -1, 0) on Dec 31, you will get
+	// Dec 1 in return). Since we do not care about the days, just the months - it's
+	// the easiest (albeit ugly) fix.
+	if previousMonth.Month() == today.Month() {
+		previousMonth = previousMonth.AddDate(0, 0, -1)
+	}
 	monthsRange := []time.Time{
 		previousMonth, today,
 	}
