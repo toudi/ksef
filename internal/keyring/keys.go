851868a88188a821bf79e0ccad195834352d5b01
Mateusz Mikołajczyk <mikolajczyk.mateusz@gmail.com>
2026-03-06 08:53:01 +0100

    wip - BROKEN BUILD (import cycles); initial support for encrypting private key files (#36)

--- internal/keyring/keys.go
+++ internal/keyring/keys.go
@@ -4,8 +4,14 @@ const (
 	AppPrefix         = "github.com/toudi/ksef"
 	keySessionTokens  = "sessionTokens"
 	KeyBackupPassword = "backup-password"
+	keyPrimaryKeyAES  = "primaryKeyAES"
+	// primary key encryption cipher - base64 encoded since zalando lib uses strings internally rather than bytes
 )
 
 func SessionTokensKey(certId string) string {
 	return keySessionTokens + "-" + certId
+}
+
+func PrimaryKeyEncryptionKey(keyId string) string {
+	return keyPrimaryKeyAES + "-" + keyId
 }
